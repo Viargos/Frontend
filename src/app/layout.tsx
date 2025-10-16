a1936df9616689bc7406fdc7ab363ebfad5fec33
Vraj Panchal <80612219+Vraj209@users.noreply.github.com>
2025-10-15 20:15:19 -0400

    add speed insite

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import {
   Geist,
   Geist_Mono,
@@ -6,44 +6,44 @@ import {
   Inter,
   Mulish,
   Outfit,
-} from "next/font/google";
-import "./globals.css";
-import AuthInitializer from "@/components/auth/AuthInitializer";
-
+} from 'next/font/google';
+import './globals.css';
+import AuthInitializer from '@/components/auth/AuthInitializer';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 const manrope = Manrope({
-  variable: "--font-manrope",
-  subsets: ["latin"],
+  variable: '--font-manrope',
+  subsets: ['latin'],
 });
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
+  variable: '--font-inter',
+  subsets: ['latin'],
 });
 
 const mulish = Mulish({
-  variable: "--font-mulish",
-  subsets: ["latin"],
+  variable: '--font-mulish',
+  subsets: ['latin'],
 });
 
 const outfit = Outfit({
-  variable: "--font-outfit",
-  subsets: ["latin"],
+  variable: '--font-outfit',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Viargos - Journey & Travel",
-  description: "Discover and share amazing journeys and travel experiences",
+  title: 'Viargos - Journey & Travel',
+  description: 'Discover and share amazing journeys and travel experiences',
 };
 
 export default function RootLayout({
@@ -56,7 +56,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${manrope.variable} ${inter.variable} ${mulish.variable} ${outfit.variable} antialiased`}
       >
-        <AuthInitializer>{children}</AuthInitializer>
+        <AuthInitializer>
+          {children}
+          <SpeedInsights />
+        </AuthInitializer>
       </body>
     </html>
   );
