3ab4761e4dfd16f5e970e56efb1f271f13ba2755
SarangTandel5112 <sarangtandel5112@gmail.com>
2026-01-18 13:37:47 +0530

    [Refactor] : Svg related isue fixed

--- src/components/discover/index.ts
+++ src/components/discover/index.ts
@@ -1,2 +1,3 @@
 export { default as JourneyDetailsModal } from './JourneyDetailsModal';
 export { default as JourneyFilters } from './JourneyFilters';
+export type { JourneyFilterState } from './JourneyFilters';
